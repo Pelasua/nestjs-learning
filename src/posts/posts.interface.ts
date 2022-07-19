e8c2ab5ff78e0602d67fc5152d1e91fc616130a2
Pelayo Suárez <pesuafer@gmail.com>
2022-07-19 16:36:59 +0200

    feat: added functionalities and swagger

---
+++ src/posts/posts.interface.ts
@@ -0,0 +1,13 @@
+import { ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
+export class PostModel {
+  @ApiPropertyOptional({ type: Number })
+  id?: number;
+  @ApiProperty({ type: String, format: 'date-time' })
+  date: Date;
+  @ApiProperty({ type: String })
+  title: string;
+  @ApiProperty({ type: String })
+  body: string;
+  @ApiProperty({ type: String })
+  category: string;
+}
